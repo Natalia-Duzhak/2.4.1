c707ac48ea295fe6792252ba049e83de0e399235
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2021-03-23 14:02:25 +0300

    Add files via upload

---
+++ UnitTest2.4/UnitTest2.4.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project2.4/Matrix.h"
+#include "../Project2.4/Matrix.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest24
+{
+	TEST_CLASS(UnitTest24)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Matrix a(0);
+			double k = 0;
+			k = a.GetN();
+			Assert::AreEqual(0,0);
+		}
+	};
+}
